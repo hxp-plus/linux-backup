0bee82056dc6a54ead61989ac715dd50bdab9bd5
hxp <hxp@hxp.plus>
2022-01-04 13:42:11 +0800

    modified:   incr_backup.sh

--- incr_backup.sh
+++ incr_backup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # A script to perform incremental backups using rsync
-
+# This script should be placed at /backup/incr_backup.sh
 set -o errexit
 set -o nounset
 set -o pipefail
 
 readonly SOURCE_DIR="/"
 readonly BACKUP_DIR="."
-readonly DATETIME="$(date '+%Y-%m-%d_%H:%M:%S')"
+readonly DATETIME="$(date '+%Y%m%d%H%M%S')"
 readonly BACKUP_PATH="${BACKUP_DIR}/${DATETIME}"
 readonly LATEST_LINK="../latest"
 
@@ -17,8 +17,8 @@ mkdir -p "${BACKUP_DIR}"
 rsync -aAHX --info=progress2 --no-i-r --delete \
   "${SOURCE_DIR}/" \
   --link-dest "${LATEST_LINK}" \
-  --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} \
+  --exclude={"/backup/*","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} \
   "${BACKUP_PATH}"
 
-rm -rf "${LATEST_LINK}"
+rm -rf "latest"
 ln -s "${BACKUP_PATH}" "latest" 
